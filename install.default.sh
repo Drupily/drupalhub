93a83014125da72aa4c1cdac0b438f4ee336e9d6
RoySegall <roy.segall@gizra.com>
2013-09-19 00:12:42 +0300

    #1: Enhance the idea platform.

---
+++ install.default.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf www
+mkdir www
+
+bash scripts/build
+bash scripts/setup
+
+cd www/sites/default
+mkdir files
+touch settings.php
+chmod 777 -R .
+
+drush si -y idea --account-pass=admin --db-url=mysql://root:root@localhost/idea
